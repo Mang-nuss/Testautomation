f14bff569a56bc05974bfbc2dc220e43a8ab7c4b
Magnus Johansson <johansson.magnus.86@gmail.com>
2021-03-25 14:05:58 +0100

    browser initiator edited

--- src/test/java/selenium/UntitledTest.java
+++ src/test/java/selenium/UntitledTest.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
@@ -22,15 +23,18 @@ import org.openqa.selenium.Keys;
 import java.util.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class UntitledTest {
-  private WebDriver driver;
+  public static WebDriver driver;
+  public static WebElement element;
   private Map<String, Object> vars;
   JavascriptExecutor js;
   @Before
   public void setUp() {
-    System.setProperty("webdriver.chrome.driver", "/Users/magnusjohansson/chromedriver");
-    driver = new ChromeDriver();
+    //System.setProperty("webdriver.chrome.driver", "/Users/magnusjohansson/chromedriver");
+    //driver = new ChromeDriver();
+    initiateBrowser("FF");
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
   }
@@ -46,10 +50,73 @@ public class UntitledTest {
     //driver.findElement(By.cssSelector("p:nth-child(4) > input")).click();
     WebElement e = driver.findElement(By.cssSelector("p:nth-child(4) > input"));
     js.executeScript("arguments[0].click();", e);
+    //assertTrue
   }
 
-  public void exploreTheManifesto() {
-    System.setProperty("webdriver.chrome.driver", "/Users/magnusjohansson/chromedriver");
-    driver = new ChromeDriver();
+  public static WebDriver initiateBrowser(String browser) {
+
+    switch (browser) {
+      case "Chrome":
+        System.setProperty("webdriver.chrome.driver", "/Users/magnusjohansson/chromedriver");
+        driver = new ChromeDriver();
+        break;
+      case "Safari":
+        driver = new SafariDriver();
+        break;
+      case "FF":
+        System.setProperty("webdriver.gecko.driver", "/Users/magnusjohansson/geckodriver");
+        driver = new FirefoxDriver();
+        break;
+    }
+
+    return driver;
+
+  }
+
+  public static void click(By by, int seconds) { //includes waiting
+
+    //driver = new ChromeDriver();
+
+    (new WebDriverWait(driver,seconds)).until(ExpectedConditions.elementToBeClickable(by)); //explicit wait
+    driver.findElement(by).click(); //can also be used for sending keys.
+  }
+
+  public static void exploreTheManifesto() throws InterruptedException {
+
+    driver = initiateBrowser("Chrome");
+    driver.get("https://agilemanifesto.org/iso/sv/manifesto.html");
+    //body > center > p:nth-child(11) > font:nth-child(16) >
+    By by = By.cssSelector("a:nth-child(83)");
+    element = driver.findElement(by);
+    Actions actions = new Actions(driver);
+    actions.moveToElement(element);
+
+    ArrayList<WebElement> elements = new ArrayList<WebElement>();
+    elements = (ArrayList<WebElement>) driver.findElements(by.cssSelector("a[href*='manifesto.html']"));
+
+    for(WebElement e : elements) { System.out.println(e.getText()); }
+
+    Thread.sleep(2000);
+    click(by,10);
+
+    element = driver.findElement(by.tagName("h1"));
+    System.out.println("text: " + element.getText());
+    assertTrue(element.isDisplayed());
+    Thread.sleep(2000);
+
+    //body > center > p:nth-child(10) > font:nth-child(2)
+    element = driver.findElement(by.cssSelector("font:nth-child(2)"));
+    element.sendKeys(Keys.CONTROL);
+    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS); //implicit wait
+
+    //driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    driver.quit();
+
+  }
+
+  //exploreTheManifesto();
+  @Test
+  public void manifestoTest() throws InterruptedException {
+    exploreTheManifesto();
   }
 }
